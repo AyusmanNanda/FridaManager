6c1359b938153f1ef7dfee0279f8200d40be3346
Ayusman Nanda <abhiseknanda799@gmail.com>
2026-05-25 02:08:49 +0530

    Add Frida status check UI integration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
 import { registerPlugin } from "@capacitor/core";
+import {useState} from "react";
+import './App.css'
 
 const Root: any = registerPlugin('Root')
 
 function App() {
+    const [status, setStatus] = useState('unknown ')
     const testBridge = async () =>{
         try{
             await Root.test()
@@ -15,18 +17,29 @@ function App() {
             alert('Bridge failed')
         }
     }
+    const checkStatus = async() => {
+        try{
+            const result = await Root.status()
+            setStatus(result.status)
+            console.log(result)
+        }
+        catch(err){
+            console.log(err)
+            setStatus('error')
+        }
+    }
     return (
         <main className="app">
             <h1>Frida Manager</h1>
             <div className="status-card">
                 <h2>Status</h2>
-                <p>Frida server: Stopped</p>
+                <p>Frida server: {status}</p>
             </div>
             <div className="actions">
                 <button>Start Frida</button>
                 <button>Stop Frida</button>
-                <button>Check Status</button>
-                <button onClick={() => testBridge()}>Test Andrid Bridge</button>
+                <button onClick={checkStatus}>Check Status</button>
+                <button onClick={testBridge}>Test Andrid Bridge</button>
             </div>
         </main>
 
